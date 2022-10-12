d8c2d733114c2ec76755927f61a9976aacf57ab2
Atanu Malik <111167455+AtanuMalik@users.noreply.github.com>
2022-10-12 21:09:36 +0530

    Update script.js

--- New Year Countdown/script.js
+++ New Year Countdown/script.js
@@ -4,21 +4,25 @@ function countDown() {
     const newYearsDate = new Date(newYear);
     const today = new Date();
 
-    const gap = (newYearsDate-today)/1000;
+    const gap = (newYearsDate - today) / 1000;
 
     const seconds = Math.floor(gap % 60);
 
-    const minutes = Math.floor(gap/60) % 60;
+    const minutes = Math.floor(gap / 60) % 60;
 
-    const hours = Math.floor(gap/3600) % 24;
+    const hours = Math.floor(gap / 3600) % 24;
 
-    const days = Math.floor(gap/3600/24);
+    const days = Math.floor(gap / 3600 / 24);
 
-
-    document.getElementById("days").innerText = days;
-    document.getElementById("hours").innerText = hours;
-    document.getElementById("minutes").innerText = minutes;
-    document.getElementById("seconds").innerText = seconds;
+    if (gap <= 0) {
+        clearInterval(myInterval);
+        document.getElementById("time").style.display = "none";
+        document.getElementById("title").innerText = "Happy New Year";
+    } else {
+        document.getElementById("days").innerText = days;
+        document.getElementById("hours").innerText = hours;
+        document.getElementById("minutes").innerText = minutes;
+        document.getElementById("seconds").innerText = seconds;
+    }
 }
-
-setInterval(countDown, 1000);
+const myInterval = setInterval(countDown, 1000);
